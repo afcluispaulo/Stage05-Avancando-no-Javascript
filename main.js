d6bc63c7c0729075cef9dae1fbc592878d07afa3
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-01-24 10:45:38 -0300

    Finalizado primeiro projeto, e começado o biscoito da sorte
    Finalizado o projeto de jogo da adivinhação, implementando a lógica de limites de tentativas, e começado a primeira tela do jogo da sorte: adicionado background, começado a primeira janela e preparativos da animação

--- main.js
+++ main.js
@@ -1,34 +1,54 @@
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 
-const randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+const total = document.querySelector("#total")
+
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+    console.log(e.key)
+    if(e.key =='Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+    }
+})
+
+console.log(randomNumber)
+
+// funções callback
 function handleTryClick(event) {
     event.preventDefault()
 
     const inputNumber = document.querySelector("#inputNumber")
 
     if(Number(inputNumber.value) == randomNumber) { 
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
-
+        toggleScreen()
         screen2.querySelector("h2").innerText =`Acertou em ${xAttempts} tentativas`
     }
 
+    total.innerText=`Total de tentativas: ${xAttempts}`
     inputNumber.value = ""
     xAttempts ++
+    
+    if (xAttempts > 11) {
+        handleResetClick()
+        screen2.querySelector("h2").innerText =`ERRO! Você excedeu a quantidade de tentativas!`
+    }
+}
+ 
+function handleResetClick() {
+    toggleScreen()
+    xAttempts = 0
+    randomNumber = Math.round(Math.random() * 10)
+    total.innerText = `Total de tentativas: ${xAttempts}`
 }
 
-// Eventos
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-// função Callback
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function() {
-    // função callback anônima
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
-    xAttempts = 1
-})
+function toggleScreen()  {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
