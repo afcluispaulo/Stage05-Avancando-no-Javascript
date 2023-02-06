2d254bd3c0664f5886429f4e9280255fe32457ed
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-02-06 11:50:51 -0300

    finalizado o projeto Biscoito da sorte

--- Biscoito da Sorte/main.js
+++ Biscoito da Sorte/main.js
@@ -9,96 +9,114 @@ const screen2 = document.querySelector(".screen2")
 const boxMsg = document.querySelector(".box")
 const boxMsg2 = document.querySelector(".box2")
 
-
 const btnTry = document.querySelector("#btnTry")
 const btnTry2 = document.querySelector("#btnTry2")
 const btn = document.querySelector('button')
 
+let countClick = 0
 
 function handleTryClick(event) {
     event.preventDefault()
+    countClick ++
+   
+    if (countClick == 1) {
+        btn.classList.add("hide")
 
-    btn.classList.add("hide")
- 
-    handleAnimationRepeat()
-    
-    handleToScreen2()
-
+        handleAnimationRepeat()
+       
+        handleToScreen2()
+    }
 }
 
-function handleTryClick2(event) {
-    event.preventDefault()
+function handleToMainScreen() {
+    countClick --
+    screen2.classList.add("hide-absolute")
+
+    screen1.classList.remove("hide")
+    btn.classList.remove("hide")
+    handleResetBox()
     
 }
 
 btnTry.addEventListener('click', handleTryClick)
-btnTry2.addEventListener('click', handleTryClick2)
+btnTry2.addEventListener('click', handleToMainScreen)
+
 
 function handleAnimationRepeat() {
 
-        setInterval(() => {
-            topImg.classList.remove("hide")
-        }, 500);
-        
-        setInterval(() => {
-            topImg.classList.add("hide")
-        }, 300);
+    setTimeout(() => {
+        topImg.classList.remove("hide")
+    }, 500);
+    
+    setTimeout(() => {
+        topImg.classList.add("hide")
+    }, 300);
 
-        setInterval(() => {
-            topCenterImg.classList.remove("hide")
-        }, 300);
+    setTimeout(() => {
+        topCenterImg.classList.remove("hide")
+    }, 300);
 
-        setInterval(() => {
-            topCenterImg.classList.add("hide")
-        }, 300);
+    setTimeout(() => {
+        topCenterImg.classList.add("hide")
+    }, 300);
 
-        setInterval(() => {
-            topRigthImg.classList.remove("hide")
-        }, 300);
+    setTimeout(() => {
+        topRigthImg.classList.remove("hide")
+    }, 300);
 
-        setInterval(() => {
-            topRigthImg.classList.add("hide")
-        }, 500);
+    setTimeout(() => {
+        topRigthImg.classList.add("hide")
+    }, 500);
 
-        setInterval(() => {
-            centerLeftImg.classList.remove("hide")
-        }, 500);      
-        
-        setInterval(() => {
-            centerLeftImg.classList.add("hide")
-        }, 500);  
-        
+
+    setTimeout(() => {
+        centerLeftImg.classList.remove("hide")
+    }, 500);      
+    
+    setTimeout(() => {
+        centerLeftImg.classList.add("hide")
+    }, 500);  
+
+    setTimeout(() => {
+        topImg.classList.add("hide")
+    }, 800);
 }
+
+function handleToScreen2() {
+    setTimeout(() => {
+        screen1.classList.add("hide")
+    }, 1300);
+
+    setTimeout(() => {
+        screen2.classList.remove("hide-absolute")
+        handleAnimationDownTopMsg()
+    }, 1300);
+ }
 
 function handleAnimationDownTopMsg() {
     
     setTimeout(() => {
-        boxMsg2.classList.add("hide-absolute")
-    }, 200);
-    boxMsg2.classList.add("hide-absolute")
-   
+        boxMsg2.classList.remove("absolute")
+    }, 300)
+
     setTimeout(() => {
         boxMsg2.classList.add("box2Animation")
-        setTimeout(() => {
-            boxMsg2.classList.remove("absolute")
-        }, 300);
+    }, 400);
+
+    setTimeout(() => {
         boxMsg2.classList.add("hide-absolute")
+    }, 700)
+
+    setTimeout(() => {
         boxMsg2.classList.remove("hide-absolute")
-    }, 300);
-    
+    }, 700);
+    setTimeout(() => {
+        boxMsg2.classList.replace("box2", "box")
+    }, 500);
 }
-boxMsg2.classList.replace("box2", "box")
 
-
-    
-function handleToScreen2() {
-    setInterval(() => {
-        screen1.classList.add("hide")
-    }, 1300);
-
-    setInterval(() => {
-        screen2.classList.remove("hide-absolute")
-        handleAnimationDownTopMsg()
-    }, 1900);
-    
+function handleResetBox() {
+    setTimeout(() => {
+        boxMsg2.classList.replace("box", "box2")
+    }, 500);
 }
