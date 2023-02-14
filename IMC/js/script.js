050ced327c0f3de181b69cd6e9b8594d6a20db11
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-02-14 16:43:42 -0300

    criado o uutil
    adicionado funções no util.js

--- IMC/js/script.js
+++ IMC/js/script.js
@@ -1,5 +1,6 @@
 import { Modal } from './modal.js'
 import { AlertError } from './alert-error.js'
+import { IMC, notANumber } from './util.js'
 
 // variáveis - variables
 const form = document.querySelector('form')
@@ -29,10 +30,4 @@ form.onsubmit = event => {
     Modal.open()
 }
 
-function notANumber(value) {
-    return isNaN(value) || value == ""
-}
 
-function IMC(weight, height) {
-    return(weight / ((height / 100) ** 2)).toFixed(2)
-}
