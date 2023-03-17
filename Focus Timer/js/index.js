e3ee1329fa5753fc1df0d7c161ae6324ad661382
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-03-16 21:53:56 -0300

    adicionado eventos de click
    Adicionados eventos d clicks iniciais nos botoes de músicas

--- Focus Timer/js/index.js
+++ Focus Timer/js/index.js
@@ -8,7 +8,7 @@ const buttonForestStop = document.querySelector('.forest-stop')
 const buttonRain = document.querySelector('.rain')
 const buttonRainStop = document.querySelector('.rain-stop')
 const buttonCoffe = document.querySelector('.coffe')
-const buttonCoffeStop = document.querySelector('coffe-stop')
+const buttonCoffeStop = document.querySelector('.coffe-stop')
 const buttonFireplace = document.querySelector('.fireplace')
 const buttonFireplaceStop = document.querySelector('.fireplace-stop')
 
@@ -16,6 +16,56 @@ buttonPlay.addEventListener('click', function(){
 
 })
 
+buttonStop.addEventListener('click', function(){
 
+})
 
+buttonAdd.addEventListener('click', function() {
+
+})
+
+buttonDecr.addEventListener('click', function() {
+
+})
+
+buttonForest.addEventListener('click', function() {
+    buttonForest.classList.add('hide')
+    buttonForestStop.classList.remove('hide')
+})
+
+buttonForestStop.addEventListener('click', function() {
+    buttonForestStop.classList.add('hide')
+    buttonForest.classList.remove('hide')
+})
+
+buttonCoffe.addEventListener('click', function() {
+    buttonCoffe.classList.add('hide')
+    buttonCoffeStop.classList.remove('hide')
+})
+ 
+buttonCoffeStop.addEventListener('click', function() {
+    buttonCoffeStop.classList.add('hide')
+    buttonCoffe.classList.remove('hide')
+})
+
+buttonRain.addEventListener('click', function() {
+    buttonRain.classList.add('hide')
+    buttonRainStop.classList.remove('hide')
+})
+
+buttonRainStop.addEventListener('click', function() {
+    buttonRainStop.classList.add('hide')
+    buttonRain.classList.remove('hide')
+})
+
+buttonFireplace.addEventListener('click', function() {
+    buttonFireplace.classList.add('hide')
+    buttonFireplaceStop.classList.remove('hide')
+})
+
+buttonFireplaceStop.addEventListener('click', function() {
+    buttonFireplaceStop.classList.add('hide')
+    buttonFireplace.classList.remove('hide')
+    
+})
 
