4a33d8b04ce83d65fb66b230050306c305f93d80
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-03-16 21:29:17 -0300

    mais estilizacoes, e iniciado javascript

---
+++ Focus Timer/js/index.js
@@ -0,0 +1,21 @@
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonAdd = document.querySelector('.add')
+const buttonDecr = document.querySelector('.decr')
+
+const buttonForest = document.querySelector('.forest')
+const buttonForestStop = document.querySelector('.forest-stop')
+const buttonRain = document.querySelector('.rain')
+const buttonRainStop = document.querySelector('.rain-stop')
+const buttonCoffe = document.querySelector('.coffe')
+const buttonCoffeStop = document.querySelector('coffe-stop')
+const buttonFireplace = document.querySelector('.fireplace')
+const buttonFireplaceStop = document.querySelector('.fireplace-stop')
+
+buttonPlay.addEventListener('click', function(){
+
+})
+
+
+
+
