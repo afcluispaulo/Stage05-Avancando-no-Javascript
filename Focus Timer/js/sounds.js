77bebaea60de9a93c89f6f29831c4f7366074c89
Luis Paulo Noronha <102933945+afcluispaulo@users.noreply.github.com>
2023-03-18 20:21:59 -0300

    adicionado funcionalidades de  áudio em js

---
+++ Focus Timer/js/sounds.js
@@ -0,0 +1,42 @@
+export default function() {
+    const forestAudio = new Audio("https://drive.google.com/file/d/1CRHkV72WUMdcqec5GT_KdsqFz0z3VAOA/view")
+
+    const rainAudio = new Audio("https://drive.google.com/file/d/1Ip8xBqAUJ-bty51Wz8JBtX_bWXCgA0P2/view")
+
+    const coffeAudio = new Audio("https://drive.google.com/file/d/1Ip8xBqAUJ-bty51Wz8JBtX_bWXCgA0P2/view")
+
+    const fireplaceAudio = new Audio("https://drive.google.com/file/d/1MakaBPxJvTa_whaSM3kEbRcxiVd1GRCB/view")
+
+    function forestAudioPlay() {
+        forestAudio.play()
+    }
+
+    function forestAudioPause() {
+        forestAudio.pause()
+    }
+
+    function rainAudioPlay() { 
+        rainAudio.play()
+    }
+
+    function rainAudioPause() {
+        rainAudio.pause()
+    }
+
+    function coffeAudioPlay() {
+        coffeAudio.play()
+    }
+
+    function coffeAudioPause() {
+        coffeAudio.pause()
+    }
+
+    function fireplaceAudioPlay() {
+        fireplaceAudio.play()
+    }
+
+    function fireplacePause() {
+        fireplaceAudio.pause()
+    }
+
+}
