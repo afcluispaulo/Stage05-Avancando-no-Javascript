91d6945456589680da101d46a8ec042ac51c4de7
Luis Paulo <luispaulo5@hotmail.com>
2023-04-03 15:10:37 -0300

    finalizado refatoração de eventos de index

---
+++ Focus Timer/js/events.js
@@ -0,0 +1,143 @@
+import {
+    buttonWhite,
+    buttonDark,
+
+    minutesDisplay,
+    secondsDisplay,
+
+    buttonPlay,
+    buttonPause,
+    buttonStop,
+    buttonAdd,
+    buttonDecr,
+
+    buttonForest,
+    buttonForestStop,
+    buttonRain,
+    buttonRainStop,
+    buttonCoffe,
+    buttonCoffeStop,
+    buttonFireplace,
+    buttonFireplaceStop,
+} from "./elements.js"
+
+export default function({
+    theme,
+    timer,
+    controls,
+    sound,
+}) {
+    buttonDark.addEventListener('click', function() {
+        theme.classList.add('dark')
+    
+        buttonDark.classList.add("hide")
+        buttonWhite.classList.remove("hide")
+    })
+    
+    buttonWhite.addEventListener('click', function() {
+        theme.classList.remove('dark')
+    
+        buttonWhite.classList.add("hide")
+        buttonDark.classList.remove("hide")
+    })
+    
+    buttonPlay.addEventListener('click', function(){
+        buttonPlay.classList.add('hide')
+        buttonPause.classList.remove('hide')
+        timer.countdown()
+        
+    })
+    
+    buttonPause.addEventListener('click', function() {
+        buttonPause.classList.add('hide')
+        buttonPlay.classList.remove('hide')
+        timer.hold()
+        
+    })
+    
+    buttonStop.addEventListener('click', function(){
+        buttonPause.classList.add('hide')
+        buttonPlay.classList.remove('hide')
+    
+        timer.reset()
+    
+    })
+    
+    buttonAdd.addEventListener('click', function() {
+        timer.add5Minutes()
+    
+    })
+    
+    buttonDecr.addEventListener('click', function() {
+        timer.dec5Minutes()
+    })
+    
+    buttonForest.addEventListener('click', function() {
+        sound.forestAudioPlay()
+        
+    
+        sound.fireplaceAudioPause()
+        sound.rainAudioPause()
+        sound.coffeAudioPause()
+    
+        controls.playForest()
+    
+    })
+    
+    buttonForestStop.addEventListener('click', function() {
+        sound.forestAudioPause()
+    
+        controls.stopForest()
+    })
+    
+    buttonRain.addEventListener('click', function() {    
+        sound.rainAudioPlay()
+    
+        sound.forestAudioPause()
+        sound.coffeAudioPause()
+        sound.fireplaceAudioPause()
+    
+        controls.playRain()
+    })
+    
+    buttonRainStop.addEventListener('click', function() {
+        sound.rainAudioPause()
+    
+        controls.stopRain()
+        
+    })
+    
+    buttonCoffe.addEventListener('click', function() {
+        sound.coffeAudioPlay()
+    
+        sound.forestAudioPause()
+        sound.rainAudioPause()
+        sound.fireplaceAudioPause()
+    
+        controls.playCoffe()
+    })
+     
+    buttonCoffeStop.addEventListener('click', function() {
+        sound.coffeAudioPause()
+    
+        controls.stopCoffe()
+    })
+    
+    buttonFireplace.addEventListener('click', function() {
+        sound.fireplaceAudioPlay()
+    
+        sound.forestAudioPause()
+        sound.rainAudioPause()
+        sound.coffeAudioPause()
+    
+        controls.playFireplace()
+    
+    })
+    
+    buttonFireplaceStop.addEventListener('click', function() {
+        sound.fireplaceAudioPause()
+    
+        controls.stopFireplace()
+        
+    })
+}
--- Focus Timer/js/index.js
+++ Focus Timer/js/index.js
@@ -1,6 +1,7 @@
 import Timer from "./timer.js"
 import sounds from "./sounds.js"
 import Controls from "./controls.js"
+import Events from "./events.js"
 
 import {
     theme,
@@ -66,117 +67,5 @@ const timer = Timer({
 
 const sound = sounds()
 
+Events({ theme, timer, controls, sound})
 
-buttonDark.addEventListener('click', function() {
-    theme.classList.add('dark')
-
-    buttonDark.classList.add("hide")
-    buttonWhite.classList.remove("hide")
-})
-
-buttonWhite.addEventListener('click', function() {
-    theme.classList.remove('dark')
-
-    buttonWhite.classList.add("hide")
-    buttonDark.classList.remove("hide")
-})
-
-buttonPlay.addEventListener('click', function(){
-    buttonPlay.classList.add('hide')
-    buttonPause.classList.remove('hide')
-    timer.countdown()
-    
-})
-
-buttonPause.addEventListener('click', function() {
-    buttonPause.classList.add('hide')
-    buttonPlay.classList.remove('hide')
-    timer.hold()
-    
-})
-
-buttonStop.addEventListener('click', function(){
-    buttonPause.classList.add('hide')
-    buttonPlay.classList.remove('hide')
-
-    timer.reset()
-
-})
-
-buttonAdd.addEventListener('click', function() {
-    timer.add5Minutes()
-
-})
-
-buttonDecr.addEventListener('click', function() {
-    timer.dec5Minutes()
-})
-
-buttonForest.addEventListener('click', function() {
-    sound.forestAudioPlay()
-    
-
-    sound.fireplaceAudioPause()
-    sound.rainAudioPause()
-    sound.coffeAudioPause()
-
-    controls.playForest()
-
-})
-
-buttonForestStop.addEventListener('click', function() {
-    sound.forestAudioPause()
-
-    controls.stopForest()
-})
-
-buttonRain.addEventListener('click', function() {    
-    sound.rainAudioPlay()
-
-    sound.forestAudioPause()
-    sound.coffeAudioPause()
-    sound.fireplaceAudioPause()
-
-    controls.playRain()
-})
-
-buttonRainStop.addEventListener('click', function() {
-    sound.rainAudioPause()
-
-    controls.stopRain()
-    
-})
-
-buttonCoffe.addEventListener('click', function() {
-    sound.coffeAudioPlay()
-
-    sound.forestAudioPause()
-    sound.rainAudioPause()
-    sound.fireplaceAudioPause()
-
-    controls.playCoffe()
-})
- 
-buttonCoffeStop.addEventListener('click', function() {
-    sound.coffeAudioPause()
-
-    controls.stopCoffe()
-})
-
-buttonFireplace.addEventListener('click', function() {
-    sound.fireplaceAudioPlay()
-
-    sound.forestAudioPause()
-    sound.rainAudioPause()
-    sound.coffeAudioPause()
-
-    controls.playFireplace()
-
-})
-
-buttonFireplaceStop.addEventListener('click', function() {
-    sound.fireplaceAudioPause()
-
-    controls.stopFireplace()
-    
-})
